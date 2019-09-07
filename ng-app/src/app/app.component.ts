4af4c64d259d6b52c3e84d5a69c4496a0e2c804b
Saurabh Palatkar <spalatkar@gmail.com>
2019-09-07 16:07:19 +0530

    Initial Commit

---
+++ ng-app/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { AfterViewInit, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent implements AfterViewInit {
+  title = 'ng-app';
+  variable = 'test';
+  ngAfterViewInit(): void {
+    console.log('vscode chrome debugging test: ', this.title);
+  }
+
+  test() {}
+}
