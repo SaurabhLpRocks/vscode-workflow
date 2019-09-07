f5b89fcdcab531f8507c7535917bc554c5e81d1f
Saurabh Palatkar <spalatkar@gmail.com>
2019-09-07 21:58:55 +0530

    API project renamed to follow c# naming std

--- CoreApi/Controllers/ValuesController.cs
+++ CoreApi/Controllers/ValuesController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace dotnet_app.Controllers
+namespace CoreApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
