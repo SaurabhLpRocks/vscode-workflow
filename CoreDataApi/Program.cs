89a1f5ddba53a889ea25e5e92df7e74d0e7b1d5f
Saurabh Palatkar <saurabh.palatkar@synapse.com>
2020-04-14 13:32:03 +0530

    feat: call data api container service from web api

--- CoreDataApi/Program.cs
+++ CoreDataApi/Program.cs
@@ -9,42 +9,16 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace CoreDataApi
-{
-    public class Program
-    {
+namespace CoreDataApi {
+    public class Program {
 
-        static readonly HttpClient client = new HttpClient();
-        public static async Task Main(string[] args)
-        {
-
-            try
-            {
-                // HttpResponseMessage response = await client.GetAsync("http://core-api-service:5000/api/values");
-                // response.EnsureSuccessStatusCode();
-                // string responseBody = await response.Content.ReadAsStringAsync();
-
-                // Console.WriteLine(response.StatusCode);
-                Console.WriteLine("XXXXXXXXXXXXXX-------Success--------XXXXXXXXXXXXXX");
-                Console.WriteLine("XXXXXXXXXXXXXX-------Success--------XXXXXXXXXXXXXX");
-                Console.WriteLine("XXXXXXXXXXXXXX-------Success--------XXXXXXXXXXXXXX");
-                Console.WriteLine("XXXXXXXXXXXXXX-------Success--------XXXXXXXXXXXXXX");
-                Console.WriteLine("XXXXXXXXXXXXXX-------Success--------XXXXXXXXXXXXXX");
-                Console.WriteLine("XXXXXXXXXXXXXX-------Success--------XXXXXXXXXXXXXX");
-                Console.WriteLine("XXXXXXXXXXXXXX-------Success--------XXXXXXXXXXXXXX");
-                Console.WriteLine("XXXXXXXXXXXXXX-------Success--------XXXXXXXXXXXXXX");
-                Console.WriteLine("XXXXXXXXXXXXXX-------Success--------XXXXXXXXXXXXXX");
-            }
-            catch (HttpRequestException e)
-            {
-                Console.WriteLine("\nException Caught!");
-                Console.WriteLine("Message :{0} ", e.Message);
-            }
-            CreateWebHostBuilder(args).Build().Run();
+        static readonly HttpClient client = new HttpClient ();
+        public static void Main (string[] args) {
+            CreateWebHostBuilder (args).Build ().Run ();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-            .UseStartup<Startup>();
+        public static IWebHostBuilder CreateWebHostBuilder (string[] args) =>
+            WebHost.CreateDefaultBuilder (args)
+            .UseStartup<Startup> ();
     }
 }
--- CoreWebApi/Program.cs
+++ CoreWebApi/Program.cs
@@ -2,23 +2,38 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace CoreWebApi
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateWebHostBuilder(args).Build().Run();
+namespace CoreWebApi {
+    public class Program {
+        static readonly HttpClient client = new HttpClient ();
+        public static async Task Main (string[] args) {
+            try {
+                HttpResponseMessage response = await client.GetAsync ("http://core-web-api-service:5002/api/values");
+                response.EnsureSuccessStatusCode ();
+                string responseBody = await response.Content.ReadAsStringAsync ();
+
+                Console.WriteLine (response.StatusCode);
+                Console.WriteLine ("XXXXXXXXXXXXXX-------Success--------XXXXXXXXXXXXXX");
+                Console.WriteLine ("XXXXXXXXXXXXXX-------Success--------XXXXXXXXXXXXXX");
+                Console.WriteLine ("XXXXXXXXXXXXXX-------Success--------XXXXXXXXXXXXXX");
+                Console.WriteLine ("XXXXXXXXXXXXXX-------Success--------XXXXXXXXXXXXXX");
+                Console.WriteLine ("XXXXXXXXXXXXXX-------Success--------XXXXXXXXXXXXXX");
+                Console.WriteLine ("XXXXXXXXXXXXXX-------Success--------XXXXXXXXXXXXXX");
+            } catch (HttpRequestException e) {
+                Console.WriteLine ("\nException Caught!");
+                Console.WriteLine ("Message :{0} ", e.Message);
+            }
+            CreateWebHostBuilder (args).Build ().Run ();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-            .UseStartup<Startup>();
+        public static IWebHostBuilder CreateWebHostBuilder (string[] args) =>
+            WebHost.CreateDefaultBuilder (args)
+            .UseStartup<Startup> ();
     }
 }
